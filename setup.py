51171ed845b602f0306c5fb95c709e024b41a8a0
"Yannis Mazzer" <yannis@mazzer.info>
2011-07-19 11:42:34 +0200

    paster structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 version = '0.2'
 shortdesc = 'Stanbol Semantic Engine: RESTful Plone Integration.'
-longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()  + "\n" + open(os.path.join("docs", "HISTORY.txt")).read()
 tests_require = ['interlude']
 
 setup(name='stanbol.plone',
@@ -15,12 +15,13 @@ setup(name='stanbol.plone',
             'License :: OSI Approved :: GNU General Public License (GPL)',
             'Operating System :: OS Independent',
             'Programming Language :: Python', 
-            'Topic :: Software Development',       
+            'Topic :: Software Development',
+            "Framework :: Plone",
       ],
       keywords='',
       author='Jens Klein    ',
       author_email='jens@bluedynamics.com',
-      url=u'',
+      url=u'http://github.com/collective/stanbol.plone',
       license='GPLv2',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
@@ -32,6 +33,8 @@ setup(name='stanbol.plone',
             'stanbol.client',
             'Plone',
       ],
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
       tests_require=tests_require,
       test_suite="stanbol.client.tests.test_suite",
       extras_require = dict(
--- src/stanbol/plone/__init__.py
+++ src/stanbol/plone/__init__.py
@@ -2,3 +2,6 @@ from zope.interface import Interface
 
 class Layer(Interface):
     """Layer Marker"""
+    
+def initialize(context):
+    """Initializer called when used as a Zope 2 product."""
